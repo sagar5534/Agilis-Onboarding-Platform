b51c317e6678ff057441d30c24fc3d144d7c4492
Sagar Patel <s.72427patel@gmail.com>
2019-05-29 15:31:45 -0400

    Setting Changed TEMP

--- Portal/Portal/settings.py
+++ Portal/Portal/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'Portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -83,6 +84,18 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
+}
+'''
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'ci',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'postgres',
+       'PORT': '5432',
+   },
 }
 
 # Password validation
