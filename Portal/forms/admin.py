0eb461ddf1cd64a75e32295ff839980798da379b
Sagar Patel <s.72427patel@gmail.com>
2019-06-07 13:53:03 -0400

    All Admin Sites Updated

--- Portal/forms/admin.py
+++ Portal/forms/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
-
+from .models import *
+from django import forms
 # Register your models here.
+
+class AddressAdmin(admin.ModelAdmin):
+
+    list_display = ('id', Address.__str__)
+    
+    search_fields = ('Street', 'City', 'Country')
+    ordering = ('id',)
+    filter_horizontal = ()
+
+    pass
+
+class AddressChoiceField(forms.ModelChoiceField):
+        def label_from_instance(self, obj):
+            return "Address {}".format( str(obj.id) + ": " + str(obj.StreetNum) + " " + obj.Street + " " + obj.City + "  ")
+
+class CompanyAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'company_name', 'site_address', 'completed')
+    list_filter = ('completed',)
+    fieldsets = ( ('Company' , {'fields': ('company_name', 'site_address', 'type', 'currentProvider')}) , ('411', {'fields': ('directory_listing','listing_name', 'category_listing', 'listing_phone', 'listing_address')}),)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email',)}
+        ),
+    )
+
+    search_fields = ('company_name', 'listing_name', '')
+    ordering = ('id', 'company_name', )
+    filter_horizontal = ()
+
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        
+        if db_field.name == 'site_address' or db_field.name == 'listing_address' :
+            return AddressChoiceField(queryset=Address.objects.all())
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    pass
+
+class UserCompLinkAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'user', 'company')
+    search_fields = ('user', 'company',)
+    ordering = ('id', 'user', )
+    filter_horizontal = ()
+
+class NumbersAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'company', 'number', 'Address_911')
+    search_fields = ('company', 'number',)
+    ordering = ('company', )
+    filter_horizontal = ()
+
+
+class ExtentionsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'company', 'ext')
+    search_fields = ('company', 'ext', 'name', )
+    ordering = ('company', )
+    filter_horizontal = ()
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(UserCompLink, UserCompLinkAdmin)
+admin.site.register(Numbers, NumbersAdmin)
+admin.site.register(Extention, ExtentionsAdmin)
--- Portal/forms/models.py
+++ Portal/forms/models.py
@@ -3,15 +3,25 @@ from login.models import MyUser
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+
 class Address(models.Model):
-    id = models.AutoField(primary_key=True)
-    Suite = models.CharField("", max_length=50, null=True, blank=True)
-    StreetNum = models.IntegerField((""), default=0, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = "addresses"
+        verbose_name = "Address"
+        ordering = ('id',)
+        
+    def __str__(self):
+        return str(self.StreetNum) + " " + self.Street + ", " + self.City + ", " + self.Prov
+
+    id = models.AutoField(("ID:"), primary_key=True)
+    Suite = models.CharField(("Suite"), max_length=50, null=True, blank=True)
+    StreetNum = models.IntegerField(("Street Number"), default=0, blank=False, null=False)
     Street = models.CharField(("Street"), default="", max_length=50, blank=False, null=False)
     City = models.CharField(("City"), default="", max_length=50, blank=False, null=False)
-    Prov = models.CharField((""), default="", max_length=50, blank=False, null=False)
+    Prov = models.CharField(("Province"), default="", max_length=50, blank=False, null=False)
     Postal = models.CharField(("Postal Code"), max_length=7, blank=False, null=False)
-    Country = models.CharField((""),default="", max_length=50, blank=False, null=False)
+    Country = models.CharField(("Country"),default="", max_length=50, blank=False, null=False)
 
     '''
     provTypes = [
@@ -37,14 +47,20 @@ class Address(models.Model):
     '''
 
 class Company(models.Model):
-    id = models.AutoField(primary_key=True)
-    completed = models.BooleanField((""), default=False, null=True)
+
+    class Meta:
+        verbose_name_plural = "Companies"
+        verbose_name = "Company"
+
+    def __str__(self):
+        return self.company_name
+
+    id = models.AutoField(("ID"), primary_key=True)
+    completed = models.BooleanField(("Completed"), default=False, null=False)
     company_name = models.CharField(("Company Name"), max_length=50, blank=False, null=True)
     site_address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=False, related_name='site_address', null=True)
-    main_phone = models.CharField(("Main Phone"), max_length=50, blank=True, null=True)
-    main_fax = models.CharField(("Main Fax"), max_length=50, blank=True, null=True)
-    type = models.CharField((""), max_length=50, null=True)
-    currentProvider = models.CharField((""), max_length=50, null=True)
+    type = models.CharField(("Type"), max_length=50, null=True)
+    currentProvider = models.CharField(("Current Provider"), max_length=50, null=True)
     #411
     directory_listing = models.CharField(("Directory Listing"), max_length=50, blank=False, null=True)
     listing_name = models.CharField(("Listing Name"), max_length=50, blank=False, null=True)
@@ -55,18 +71,38 @@ class Company(models.Model):
     #Inserting null into blanks for phones
     #def ...
 
+class CompanyAddressLink(models.Model):
+    id = models.AutoField(primary_key=True)
+    Address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=False, null=False)
+    Company = models.ForeignKey(Company, on_delete=models.CASCADE,  blank=False, null=False)
+
 class UserCompLink(models.Model):
+
+    class Meta:
+        verbose_name_plural = "User & Company Relations"
+        verbose_name = "User & Company Relation"
+
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, blank=False, null=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE,  blank=False, null=False)
 
 class Numbers(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Numbers"
+        verbose_name = "Number"
+    
     id = models.AutoField(primary_key=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, blank=False, null=False)
     number = PhoneNumberField((""), blank=False, null=False)
     Address_911 = models.ForeignKey(Address, on_delete=models.CASCADE, blank=False, null=True)
 
 class Extention(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Extentions"
+        verbose_name = "Extention"
+
     id = models.AutoField(primary_key=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, blank=False, null=False)
     ext = models.IntegerField((""), blank=False, null=False)
