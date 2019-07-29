4947dbed66b9c6b9ee7d974c0c6c3b15110f0acb
Sagar Patel <s.72427patel@gmail.com>
2019-07-29 09:34:21 -0400

    added sql password

--- Portal/Portal/settings.py
+++ Portal/Portal/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Portal',
-        'USER': 'root',
-        'PASSWORD': '72427Patel',
+        'USER': 'webdev',
+        'PASSWORD': '>xy@YW`S3VL>',
         'HOST': 'localhost',
         'PORT': '3306',
     }
